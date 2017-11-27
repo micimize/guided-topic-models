e662e1be0c3d3f5da002d1bb430233dedc73488c
micimize <rosenthalm93@gmail.com>
2017-11-27 13:17:01 -0600

    simple SASS imports

--- web/webpack.config.ts
+++ web/webpack.config.ts
@@ -1,29 +1,23 @@
-import * as webpack from 'webpack'
-import * as path from 'path'
-import * as HtmlWebpackPlugin from 'html-webpack-plugin'
-import * as ExtractTextPlugin from 'extract-text-webpack-plugin'
-
-const context = path.resolve(__dirname, 'src')
+import * as webpack from "webpack";
+import * as path from "path";
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const config: webpack.Configuration = {
-  context,
-
   entry: [
-    'react-hot-loader/patch',
-    './index.tsx',
+    "react-hot-loader/patch",
+    "./src/index.tsx",
   ],
-
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: "bundle.js",
   },
 
   // Enable sourcemaps for debugging webpack's output.
-  devtool: 'source-map',
+  devtool: "source-map",
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: ['.scss', '.ts', '.tsx', '.js']
+    extensions: [".ts", ".tsx", ".js"]
   },
 
   plugins: [
@@ -38,62 +32,29 @@ const config: webpack.Configuration = {
 
   module: {
     loaders: [
-      {
-        test: /\.scss$/,
-        include: context,
-        use: ExtractTextPlugin.extract({
-          use: [
-            // load component css via css modules with local scoping
-            {
-              loader: "css-loader",
-              query: {
-                modules: true,
-                importLoaders: 1,
-                localIdentName: "[name]__[local]",
-                minimize: false,
-                sourceMap: true,
-              },
-            },
-            "postcss-loader", // config is in postcss.config.js
-          ],
-        })
-      },
       // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
       {
         test: /\.tsx?$/,
-        include: context,
-        exclude: path.resolve(__dirname, 'node_modules'),
-        use: [
-          'react-hot-loader/webpack',
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [ [ 'env', { 'modules': false } ], 'react' ],
-              plugins: [
-                [
-                  'react-css-modules',
-                  {
-                    context,
-                    'generateScopedName': '[name]__[local]'
-                  }
-                ]
-              ]
-            }
-          },
-          'awesome-typescript-loader',
+        loaders: [
+          "react-hot-loader/webpack",
+          "awesome-typescript-loader"
         ],
+        exclude: path.resolve(__dirname, 'node_modules'),
+        include: path.resolve(__dirname, "src"),
       },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {
-        enforce: 'pre',
+        enforce: "pre",
         test: /\.js$/,
-        loader: 'source-map-loader'
+        loader: "source-map-loader"
       },
     ]
   },
+
   devServer: {
     hot: true
   }
+
 };
 
 export default config;
